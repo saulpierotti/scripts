8399bba327a26ffeddbeba26cde9447311aeb5a0
Saul Pierotti <39513558+saulpierotti@users.noreply.github.com>
2020-01-25 10:45:16 +0100

    Add files via upload

---
+++ bash/list_pkgs.sh
@@ -0,0 +1,13 @@
+DATE=$(date "+%Y.%m.%d-%H.%M")
+MY_PATH=~saul/.pkg_history
+
+OFF=$MY_PATH/pkglist_off-$DATE.txt
+AUR=$MY_PATH/pkglist_aur-$DATE.txt
+
+pacman -Qqen > $OFF
+pacman -Qqem > $AUR
+
+# remove older than 30 days
+echo "Removing the following old log files..."
+find $MY_PATH -type f -name 'pkglist*' -mtime +30
+find $MY_PATH -type f -name 'pkglist*' -mtime +30 -exec rm {} \;
