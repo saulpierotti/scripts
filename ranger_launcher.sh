e8755e7726d3eb5796c1a8c09033581dcf03b5df
saulpierotti <saulpierotti@gmail.com>
2020-04-15 06:00:14 +0200

    .

--- ranger_launcher.sh
+++ ranger_launcher.sh
@@ -1,11 +1,10 @@
 # lauches ranger so that when I close it i moved to the new dir
+# in order to make cd work I need to source it, not run
 
 tmp="$(mktemp)"
 ranger --choosedir="$tmp" "$@"
 if [ -f "$tmp" ]; then
 	dir="$(cat "$tmp")"
 	rm -f "$tmp"
-	if [ -d "$dir" ] && [ "$dir" != "$(pwd)" ]; then
-		cd /
-	fi
+	[ -d "$dir" ] && [ "$dir" != "$(pwd)" ] && cd "$dir"
 fi
