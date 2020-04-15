99a44aa1bd4ebab243fb0908915655124d3423b0
saulpierotti <saulpierotti@gmail.com>
2020-04-15 05:56:20 +0200

    .

--- ranger_launcher.sh
+++ ranger_launcher.sh
@@ -1,1 +1,11 @@
-$SHELL --exec ranger
+# lauches ranger so that when I close it i moved to the new dir
+
+tmp="$(mktemp)"
+ranger --choosedir="$tmp" "$@"
+if [ -f "$tmp" ]; then
+	dir="$(cat "$tmp")"
+	rm -f "$tmp"
+	if [ -d "$dir" ] && [ "$dir" != "$(pwd)" ]; then
+		cd /
+	fi
+fi
