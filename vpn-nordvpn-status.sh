787ec048b96c2449091d6b841c454a069efa3a6e
saulpierotti <saulpierotti@gmail.com>
2020-11-02 20:34:40 +0300

    .

--- vpn-nordvpn-status.sh
+++ vpn-nordvpn-status.sh
@@ -5,7 +5,7 @@ STATUS=$(nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2)
 if [ "$STATUS" = "Connected" ]; then
     my_country=$(nordvpn status | grep Country | tr -d ' ' | cut -d ':' -f2)
     my_ip=$(nordvpn status | grep IP | tr -d ' ' | cut -d ':' -f2)
-    echo $my_ip" ("$my_country")"
+    echo "  $my_country"
 else
-    echo "NordVPN disconnected"
+    echo ""
 fi
