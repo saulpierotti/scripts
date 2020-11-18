b1fb53b3b2688076fcb6d20ac7632f85e1ba7696
saulpierotti <saulpierotti@gmail.com>
2020-11-18 20:48:37 +0300

    .

--- to_dotfiles.sh
+++ to_dotfiles.sh
@@ -5,14 +5,25 @@
 # Puts an absolute symlink to the moved file in the original location
 # DO NOT USE PATHS WITH WHITESPACES
 
-dotfiles_repo_folder="$HOME/.dotfiles"
+if [ "$(whoami)" = "root" ]; then
+    USER_HOME="$(eval echo ~${SUDO_USER})"
+else
+    USER_HOME="$HOME"
+fi
+
+dotfiles_repo_folder="$USER_HOME/.dotfiles"
 
 if [ ! -d "$dotfiles_repo_folder" ]; then
     echo "$dotfiles_repo_folder does not exist! Aborting..."
     exit 0
 fi
 
-original_file_fullpath="$(pwd)/$1" # get the full path
+if [ -h "$1" ]; then
+    echo "$1 is a symlink! Aborting..."
+    exit 0
+fi
+
+original_file_fullpath="$(realpath $1)" # get the full path
 
 if `echo "$original_file_fullpath"|grep -q ' '`; then
     echo "$original_file_fullpath contains whitespaces! Aborting..."
@@ -29,10 +40,15 @@ if [ ! -f "$original_file_fullpath" ]; then
     exit 0
 fi
 
+if [ ! -w "$original_file_fullpath" ]; then
+    echo "Insufficient permissions! Run as sudo. Aborting..."
+    exit 0
+fi
+
 echo "Processing the file:"
 echo "$original_file_fullpath"
 
-new_file_location="$dotfiles_repo_folder/$original_file_fullpath"
+new_file_location="$dotfiles_repo_folder$original_file_fullpath"
 echo "Final path where to place it:"
 echo "$new_file_location"
 
