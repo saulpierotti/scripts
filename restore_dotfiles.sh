19c7286c928930d8927b28113c3d2cb3b00fa5ac
saulpierotti <saulpierotti@gmail.com>
2020-11-29 22:49:26 +0300

    .

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+# Substitute all the dotfiles in the system to the ones saved in .dotifiles
+# This is useful for recreating my system configuration
+
+dotfiles_root="$HOME/.dotfiles"
+# include hidden files in globbing
+shopt -s dotglob
+
+explore() {
+    for element in "$1"/*; do
+        if [ -f "$element" ]; then
+            echo "$element"
+        elif [ -d "$element" ]; then
+            explore "$element"
+        fi
+    done
+}
+
+explore "$dotfiles_root"
