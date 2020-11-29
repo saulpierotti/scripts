04269defe26e7fc83f22d2fbd5a0877586fc5c08
saulpierotti <saulpierotti@gmail.com>
2020-11-29 15:55:14 +0300

    .

--- compiler.sh
+++ compiler.sh
@@ -7,31 +7,31 @@ file="$1"
 base="${file%.*}"
 filename="${base##*/}"
 
-if [ ${file: -3} == ".md" ]
-then
-	echo "Compiling md file to pdf..."
-	pandoc --citeproc -V geometry:margin=1in  $file -o $base.pdf
-elif [ ${file: -4} == ".tex" ]
-then
-	if [ -d "metafiles" ]
-	then
-		pdflatex -output-directory=metafiles $file
-		mv metafiles/$filename.pdf $base.pdf
-		mv metafiles/$filename.bcf $base.bcf
-
-	elif [ ! -d "metafiles" ]
-	then
-		echo "Compiling tex file to pdf..."
-		pdflatex -interaction=nonstopmode -synctex=1 $file
-	fi
-elif [ ${file: -2} == ".R" ]
-then
-	echo "Executing R script..."
-	Rscript $file
-elif [ ${file: -3} == ".py" ]
-then
-	echo "Executing python script..."
-	python $file
+if [ "${file: -3}" == ".md" ]; then
+    echo "Compiling md file to pdf..."
+    command="pandoc $file
+    --citeproc
+    -V colorlinks
+    -V geometry:a4paper
+    -V geometry:margin=1in
+    -o $base.pdf"
+elif [ "${file: -4}" == ".tex" ]; then
+    if [ -d "metafiles" ]; then
+        command="pdflatex -output-directory=metafiles $file;mv
+        metafiles/$filename.pdf $base.pdf; mv metafiles/$filename.bcf
+        $base.bcf"
+    elif [ ! -d "metafiles" ]; then
+        echo "Compiling tex file to pdf..."
+        command="pdflatex -interaction=nonstopmode -synctex=1 $file"
+    fi
+elif [ "${file: -2}" == ".R" ]; then
+    echo "Executing R script..."
+    command="Rscript $file"
+elif [ "${file: -3}" == ".py" ]; then
+    echo "Executing python script..."
+    command="python $file"
 else
-	echo "Filetype not recognized: I do not know what to do"
+    echo "Filetype not recognized: I do not know what to do"
 fi
+
+$command
