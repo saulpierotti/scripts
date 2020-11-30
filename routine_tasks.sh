1a220c7e27d213e75f76c951f960ff8acb106e2b
saulpierotti <saulpierotti@gmail.com>
2020-11-30 03:29:38 +0300

    routine_tasks.sh

--- routine_tasks.sh
+++ routine_tasks.sh
@@ -5,7 +5,6 @@ commit_message=routine_tasks.sh
 git_repos="
     $HOME/notebook
     $HOME/.dotfiles
-    $HOME/.bioscripts
     $HOME/.scripts
     "
 
@@ -14,7 +13,11 @@ echo "Routine task executor script"
 for folder in $git_repos; do
     echo "syncing $folder..."
     cd "$folder" || return
-    git_update.sh "$commit_message"
+    git add -A
+    git commit -m "$commit_message" .
+    # in this order so to leave origin as the push default
+    git push -u backup
+    git push -u origin
 done
 
 # end confirmation
